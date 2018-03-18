7934536e39ffa37974504c41998915ddd3e1d1b2
Warren MacEvoy <wmacevoy@gmail.com>
2018-03-18 11:50:37 -0600

    due does not have progmem

--- src/ArduinoUnit.h
+++ src/ArduinoUnit.h
@@ -52,12 +52,12 @@
 // #endif
 // #endif
 
-// // Workaround for Arduino Due
-// #if defined(__arm__) && !defined(PROGMEM)
-// #define PSTR(s) s
-// #define memcpy_P(a, b, c) memcpy(a, b, c)
-// #define strlen_P(a) strlen(a)
-// #endif
+// Workaround for Arduino Due
+#if defined(__arm__) && !defined(PROGMEM)
+#define PSTR(s) s
+#define memcpy_P(a, b, c) memcpy(a, b, c)
+#define strlen_P(a) strlen(a)
+#endif
 
 #include <ArduinoUnitUtility/Compare.h>
 #include <ArduinoUnitUtility/FakeStream.h>

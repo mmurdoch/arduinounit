f6fb6bb73ddef718d49f4047bfab61b0afffbd9b
Warren MacEvoy <wmacevoy@gmail.com>
2018-04-02 18:21:25 -0600

    missing header

---
+++ src/ArduinoUnitUtility/ArduinoUnitMock.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "ArduinoUnitMockTime.h"
+#include "ArduinoUnitMockPrintable.h"
+#include "ArduinoUnitMockWString.h"
+#include "ArduinoUnitMockPrint.h"
+#include "ArduinoUnitMockStream.h"
